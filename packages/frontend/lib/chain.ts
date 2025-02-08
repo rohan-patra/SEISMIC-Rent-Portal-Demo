c2276b6954ba1b6a8fff881e0a7335272b2b4d56
Ravi Riley <raviriley@gmail.com>
2025-02-08 18:40:26 -0500

    Update chain with env vars

--- packages/frontend/lib/chain.ts
+++ packages/frontend/lib/chain.ts
@@ -1,28 +1,22 @@
-import { createPublicClient, fallback, http } from "viem";
 import { defineChain } from "viem";
+
+const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL as string;
+const EXPLORER_URL = process.env.NEXT_PUBLIC_EXPLORER_URL as string;
 
 export const seismicDevnet = defineChain({
   id: 1337,
   name: "Seismic Devnet",
   rpcUrls: {
-    default: { http: ["http://48.211.212.30:8545"] },
+    default: { http: [RPC_URL] },
   },
   nativeCurrency: { name: "Ether", symbol: "ETH", decimals: 18 },
-  // blockExplorers: {
-  //   default: { name: "TBD", url: "https://seismic.systems/explorer" },
-  // },
+  blockExplorers: {
+    default: { name: "Seismic Devnet Explorer", url: EXPLORER_URL },
+  },
   // contracts: {
   //   multicall3: {
   //     address: "TBD",
   //     blockCreated: TBD,
   //   },
   // },
-});
-
-export const publicClient = createPublicClient({
-  batch: {
-    multicall: true,
-  },
-  chain: seismicDevnet,
-  transport: fallback([http("http://48.211.212.30:8545")]),
 });
